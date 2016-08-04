157a77a942b7980ae4ec2992df978a9c0ffdb39f
Administrator <Administrator@HO>
2016-08-04 13:49:57 +0900

    규호:web.xml cloudcontroller 추가

--- HoneyComb/src/cloudaction/CloudController.java
+++ HoneyComb/src/cloudaction/CloudController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CloudControllerUsingURI extends HttpServlet{
+public class CloudController extends HttpServlet{
 
 	private Map commandMap = new HashMap();
 
